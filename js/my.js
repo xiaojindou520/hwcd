65df010ad7d3fb1ad21a6fcf2ca762abd95c6c5b
xiaojindou520 <9986720@qq.com>
2018-07-19 16:57:08 +0800

    2018.7.19

--- js/my.js
+++ js/my.js
@@ -39,6 +39,13 @@ $(document).ready(function(){
         $(this).slideUp();
     });
 
+    //产品分类菜单
+    $(".catemenu dt").click(function () {
+        $(".catemenu dd").stop().slideUp();
+        $(".catemenu dt span").css("transform","rotate(0deg)");
+        $(this).closest("dl").find("dd").stop().slideDown();
+        $(this).find("span").css("transform","rotate(180deg)")
+    });
     //搜索栏
     $("#open").click(function () {
         $(".search").show();
@@ -52,6 +59,20 @@ $(document).ready(function(){
         },"",function () {
             $(this).hide();
         })
+    });
+
+    //产品图片展示
+    $(".prospic ul li:first").css("border","#333 solid 1px");
+    $(".prospic ul li").mouseenter(function () {
+        var src1 = $(this).find("img").attr("src");
+        //console.log(src);
+        $(this).css("border","#333 solid 1px").siblings().css("border","#fff solid 1px");
+        $(".propic").find("img").attr("src",src1);
+    });
+
+    //颜色规格选择
+    $(".yanse ul li span").click(function () {
+        $(this).css("border","#333 solid 1px").parent().siblings().find("span").css("border","#f5f5f5 solid 1px");
     });
 });
 //禁止右键
